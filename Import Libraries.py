35129e3673ef6db8e64a883c445413a271ccfd8c
Rutuja Patil <107934946+vrushivar@users.noreply.github.com>
2023-01-11 13:15:03 +0530

    Create Import Libraries.py

---
+++ Import Libraries.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+import math,statistics
+import statsmodels.formula.api as smf
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pickle
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,mean_absolute_error  
